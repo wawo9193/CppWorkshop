dd7e12f37ba8e71514d0909989766f542609c3ea
Wayne <wayne.wood@colorado.edu>
2021-02-04 21:39:32 -0700

    cleaned up

--- HW/HW1/Player.cpp
+++ HW/HW1/Player.cpp
@@ -12,21 +12,3 @@ void Player::ChangePoints(const int x) {
 void Player::SetPosition(Position pos) {
     this->pos_ = pos;
 }
-
-// std::string Player::ToRelativePosition(Position other) {
-//     if (other.get_cols()>4 || other.get_rows()>4) return "you shall not pass";
-//     if (this->pos_==other) return "stay";
-
-//     std::vector<int> diff = other - this->pos_;
-//     if (diff[0]<0) return "down";
-//     if (diff[0]>0) return "up";
-//     if (diff[1]<0) return "left";
-//     if (diff[1]>0) return "right";
-
-//     return "shaddup";
-// }
-
-// std::ostream& operator<<(std::ostream& os, Player& p) {
-//     os << "Name: " << this->name_ << "\tPoints: " << this->points_ << "\n";
-//     return os;
-// }
